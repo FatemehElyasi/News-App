21e7bf03a3a398d86e4ad6832ceab13d2bc4980b
Fatemelyasi <fatemelyasii@gmail.com>
2025-07-01 10:03:18 +0430

    Revert "fix some bugs add default for color and typography,"
    This reverts commit b1d16c97072aba97bcc42a4ff1c1a7e044f3e76b.

--- app/src/main/java/ir/fatemelyasi/news/view/screens/articleDetailScreen/ArticleDetailScreen.kt
+++ app/src/main/java/ir/fatemelyasi/news/view/screens/articleDetailScreen/ArticleDetailScreen.kt
@@ -1,6 +1,7 @@
 package ir.fatemelyasi.news.view.screens.articleDetailScreen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +11,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -59,13 +59,12 @@ fun ArticleDetailScreen(
                 .padding(20.dp)
                 .background(colors.surface)
         ) {
-            Icon(
+            Image(
+                painter = painterResource(id = R.drawable.back),
+                contentDescription = "back",
                 modifier = Modifier
                     .padding(top = 40.dp, bottom = 20.dp)
-                    .clickable { popBackStack() },
-                painter = painterResource(id = R.drawable.back),
-                contentDescription = "Back",
-                tint = (colors.onPrimary)
+                    .clickable { popBackStack() }
             )
 
             AsyncImage(
--- app/src/main/java/ir/fatemelyasi/news/view/ui/theme/Theme.kt
+++ app/src/main/java/ir/fatemelyasi/news/view/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
+
 data class CustomColors(
     val primary: Color,
     val onPrimary: Color,
@@ -25,34 +26,19 @@ data class CustomColors(
     val onError: Color,
     val secondaryContainer: Color,
     val onSecondaryContainer: Color,
+    val tertiary: Color,
+    val onTertiary: Color,
+    val tertiaryContainer: Color,
     val onTertiaryContainer: Color,
     val errorContainer: Color,
     val onErrorContainer: Color,
+    val inverseSurface: Color,
+    val inverseOnSurface: Color,
+    val surfaceVariant: Color,
+    val onSurfaceVariant: Color,
     val outline: Color,
-    val onSurfaceVariant: Color
+    val inversePrimary: Color,
 )
-
-val LightColorScheme = CustomColors(
-    primary = Blue40,
-    onPrimary = Black,
-    primaryContainer = White,
-    onPrimaryContainer = Blue10,
-    secondary = DarkBlue40,
-    onSecondary = White,
-    secondaryContainer = DarkBlue90,
-    onSecondaryContainer = DarkBlue10,
-    onTertiaryContainer = Yellow10,
-    error = Red40,
-    onError = White,
-    errorContainer = Red90,
-    onErrorContainer = Red10,
-    background = Grey99,
-    onBackground = Grey10,
-    surface = Grey99,
-    onSurface = Grey10,
-    outline = BlueGrey50,
-    onSurfaceVariant = BlueGrey30,
-    )
 
 val DarkColorScheme = CustomColors(
     primary = Blue80,
@@ -63,6 +49,9 @@ val DarkColorScheme = CustomColors(
     onSecondary = DarkBlue20,
     secondaryContainer = DarkBlue30,
     onSecondaryContainer = DarkBlue90,
+    tertiary = Yellow80,
+    onTertiary = Yellow20,
+    tertiaryContainer = Yellow30,
     onTertiaryContainer = Yellow90,
     error = Red80,
     onError = Red20,
@@ -72,16 +61,48 @@ val DarkColorScheme = CustomColors(
     onBackground = Grey90,
     surface = Grey10,
     onSurface = Grey80,
-    outline = BlueGrey60,
+    inverseSurface = Grey90,
+    inverseOnSurface = Grey20,
+    surfaceVariant = BlueGrey30,
     onSurfaceVariant = BlueGrey80,
+    outline = BlueGrey60,
+    inversePrimary = BlueGrey60,
 )
 
+val LightColorScheme = CustomColors(
+    primary = Blue40,
+    onPrimary = Black,
+    primaryContainer = White,
+    onPrimaryContainer = Blue10,
+    inversePrimary = Blue80,
+    secondary = DarkBlue40,
+    onSecondary = White,
+    secondaryContainer = DarkBlue90,
+    onSecondaryContainer = DarkBlue10,
+    tertiary = Yellow40,
+    onTertiary = White,
+    tertiaryContainer = Yellow90,
+    onTertiaryContainer = Yellow10,
+    error = Red40,
+    onError = White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = Grey99,
+    onBackground = Grey10,
+    surface = Grey99,
+    onSurface = Grey10,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = BlueGrey90,
+    onSurfaceVariant = BlueGrey30,
+    outline = BlueGrey50,
+)
 
 val LocalCustomColors = staticCompositionLocalOf<CustomColors> {
-    LightColorScheme
+    error("No CustomColors provided")
 }
 val LocalCustomTypography = staticCompositionLocalOf<AppTypography> {
-    ComposeTypography
+    error("No typography provided")
 }
 
 @Composable
@@ -104,4 +125,3 @@ fun ComposeTheme(
         }
     }
 }
-
